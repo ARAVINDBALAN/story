73d8a0e4277d917390c0a6cc551e829d302624c4
Aravind Balakrishnan <aravindbalan222@gmail.com>
2018-07-17 13:23:49 +0530

    happy

--- shortstorylife/urls.py
+++ shortstorylife/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 urlpatterns = [
+	url(r'^submit',views.submit,name="submit"),
     url(r'^$',views.mainpage,name="mainpage" ),
-    url(r'^submit/',views.submit,name="submit"),
 ]
